653dd81563709e2c5d7295351bde3af194cf83f8
Junwang Zhao <zhjwpku@gmail.com>
2026-06-30 16:55:10 +0000

    refactor: move datum conversion helpers to common

--- src/common/datum_convert.cc
+++ src/common/datum_convert.cc
@@ -1,9 +1,15 @@
 #include "common/datum_convert.h"
 
+#include <cstring>
+#include <memory>
 #include <optional>
 #include <string>
 
 #include <arrow/array.h>
+#include <arrow/array/builder_base.h>
+#include <arrow/scalar.h>
+
+#include "common/status.h"
 
 extern "C" {
 #include "postgres.h"
@@ -89,8 +95,8 @@ std::int64_t TimestampMicros(const arrow::Array& array, std::int64_t offset) {
 
 }  // namespace
 
-Result<std::uintptr_t> ConvertValue(const arrow::Array& array, std::int64_t offset,
-                                    unsigned int pg_type, bool& is_null) {
+Result<Datum> ConvertValue(const arrow::Array& array, std::int64_t offset, Oid pg_type,
+                           bool& is_null) {
   if (array.IsNull(offset)) {
     is_null = true;
     return static_cast<Datum>(0);
@@ -158,6 +164,88 @@ Result<std::uintptr_t> ConvertValue(const arrow::Array& array, std::int64_t offs
       MakeError(ERRCODE_FEATURE_NOT_SUPPORTED,
                 "unsupported pgiceberg column conversion from Arrow type " +
                     array.type()->ToString()));
+}
+
+Result<std::shared_ptr<arrow::Scalar>> ScalarFromDatum(Datum datum,
+                                                       const arrow::DataType& type) {
+  switch (type.id()) {
+    case arrow::Type::BOOL:
+      return std::make_shared<arrow::BooleanScalar>(DatumGetBool(datum));
+    case arrow::Type::INT16:
+      return std::make_shared<arrow::Int16Scalar>(DatumGetInt16(datum));
+    case arrow::Type::INT32:
+    case arrow::Type::DATE32: {
+      if (type.id() == arrow::Type::DATE32) {
+        const auto days =
+            DatumGetDateADT(datum) + POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE;
+        return std::make_shared<arrow::Date32Scalar>(days);
+      }
+      return std::make_shared<arrow::Int32Scalar>(DatumGetInt32(datum));
+    }
+    case arrow::Type::INT64:
+      return std::make_shared<arrow::Int64Scalar>(DatumGetInt64(datum));
+    case arrow::Type::FLOAT:
+      return std::make_shared<arrow::FloatScalar>(DatumGetFloat4(datum));
+    case arrow::Type::DOUBLE:
+      return std::make_shared<arrow::DoubleScalar>(DatumGetFloat8(datum));
+    case arrow::Type::STRING:
+    case arrow::Type::LARGE_STRING: {
+      char* text_value = TextDatumGetCString(datum);
+      return std::make_shared<arrow::StringScalar>(std::string(text_value));
+    }
+    case arrow::Type::TIMESTAMP: {
+      const auto micros = DatumGetTimestamp(datum) + kPostgresUnixEpochOffsetMicros;
+      return std::make_shared<arrow::TimestampScalar>(
+          micros, std::static_pointer_cast<arrow::TimestampType>(
+                      std::const_pointer_cast<arrow::DataType>(type.shared_from_this())));
+    }
+    default:
+      return std::unexpected(
+          MakeError(ERRCODE_FEATURE_NOT_SUPPORTED,
+                    "unsupported pgiceberg INSERT Arrow type " + type.ToString()));
+  }
+}
+
+Status AppendDatum(arrow::ArrayBuilder& builder, Datum value, bool is_null,
+                   const arrow::DataType& type) {
+  if (is_null) {
+    return FromArrowStatus(builder.AppendNull(), "append Arrow NULL");
+  }
+  PGICEBERG_ASSIGN_OR_RETURN(auto scalar, ScalarFromDatum(value, type));
+  return FromArrowStatus(builder.AppendScalar(*scalar), "append Arrow scalar");
+}
+
+Result<bool> DatumEquals(Datum left, Datum right, Oid pg_type) {
+  switch (pg_type) {
+    case BOOLOID:
+      return DatumGetBool(left) == DatumGetBool(right);
+    case INT2OID:
+      return DatumGetInt16(left) == DatumGetInt16(right);
+    case INT4OID:
+      return DatumGetInt32(left) == DatumGetInt32(right);
+    case INT8OID:
+      return DatumGetInt64(left) == DatumGetInt64(right);
+    case FLOAT4OID:
+      return DatumGetFloat4(left) == DatumGetFloat4(right);
+    case FLOAT8OID:
+      return DatumGetFloat8(left) == DatumGetFloat8(right);
+    case DATEOID:
+      return DatumGetDateADT(left) == DatumGetDateADT(right);
+    case TIMESTAMPOID:
+      return DatumGetTimestamp(left) == DatumGetTimestamp(right);
+    case TIMESTAMPTZOID:
+      return DatumGetTimestampTz(left) == DatumGetTimestampTz(right);
+    case TEXTOID:
+    case VARCHAROID:
+    case BPCHAROID: {
+      char* left_text = TextDatumGetCString(left);
+      char* right_text = TextDatumGetCString(right);
+      return std::strcmp(left_text, right_text) == 0;
+    }
+    default:
+      return std::unexpected(MakeError(ERRCODE_FEATURE_NOT_SUPPORTED,
+                                       "unsupported pgiceberg row identity type"));
+  }
 }
 
 }  // namespace pgiceberg
--- src/common/datum_convert.h
+++ src/common/datum_convert.h
@@ -1,16 +1,32 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
+
+extern "C" {
+#include "postgres.h"
+}
 
 #include "common/status.h"
 
 namespace arrow {
 class Array;
-}
+class ArrayBuilder;
+class DataType;
+class Scalar;
+}  // namespace arrow
 
 namespace pgiceberg {
 
-Result<std::uintptr_t> ConvertValue(const arrow::Array& array, std::int64_t offset,
-                                    unsigned int pg_type, bool& is_null);
+Result<Datum> ConvertValue(const arrow::Array& array, std::int64_t offset, Oid pg_type,
+                           bool& is_null);
+
+Result<std::shared_ptr<arrow::Scalar>> ScalarFromDatum(Datum value,
+                                                       const arrow::DataType& type);
+
+Status AppendDatum(arrow::ArrayBuilder& builder, Datum value, bool is_null,
+                   const arrow::DataType& type);
+
+Result<bool> DatumEquals(Datum left, Datum right, Oid pg_type);
 
 }  // namespace pgiceberg
